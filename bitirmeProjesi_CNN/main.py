1db62c9a0f69ce2e42e30cc0984aaa33683ae376
Melisa Yasak <melisa.yasak@bil.omu.edu.tr>
2024-04-28 15:53:51 +0300

    Update bitirme projesi

--- bitirmeProjesi_CNN/main.py
+++ bitirmeProjesi_CNN/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
-from fastapi import File, UploadFile 
+from fastapi import FastAPI, Request, File, UploadFile 
 from tensorflow.keras.preprocessing import image
 import numpy as np
-import io
+from fastapi.responses import JSONResponse
+from PIL import Image
 from tensorflow.keras.models import load_model
+from fastapi.responses import HTMLResponse
 
 
 app = FastAPI()
@@ -18,20 +19,44 @@ def about():
     return {"message": "Melisa YASAK"}
 
 
+@app.get("/cnn", response_class=HTMLResponse)
+async def cnn(request: Request):
+    return HTMLResponse(open("C:\\Users\\melis\\WTech_YZE\\bitirmeProjesi_CNN\\cnn.html", "r").read())
+
+
+@app.get("/ann", response_class=HTMLResponse)
+async def ann(request: Request):
+    return HTMLResponse(open("C:\\Users\\melis\\WTech_YZE\\bitirmeProjesi_CNN\\ann.html", "r").read())
+
+@app.get("/object_detection", response_class=HTMLResponse)
+async def object_detection(request: Request):
+    return HTMLResponse(open("C:\\Users\\melis\\WTech_YZE\\bitirmeProjesi_CNN\\object_detection.html", "r").read())
+
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return HTMLResponse(open("C:\\Users\\melis\\WTech_YZE\\bitirmeProjesi_CNN\\index.html", "r").read())
+
+
+
 @app.post("/predict_image/")
 async def predict_image(file: UploadFile = File(...)):
     load_ann = load_model(r'C:\Users\melis\WTech_YZE\bitirmeProjesi_CNN\image classification with cnn\models\dog-cat2.h5')
-    contents = await file.read()
-    img = image.load_img(io.BytesIO(contents), target_size=(240, 240))
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = load_ann.predict(img_array)
+    with open("temp_image.jpg", "wb") as f:
+        f.write(await file.read())
+        
+    test_image = Image.open("temp_image.jpg")
+    test_image = test_image.resize((240, 240))
+    
+    test_image = np.array(test_image)
+    test_image = np.expand_dims(test_image, axis=0) / 255.0
+    
+    prediction = load_ann.predict(test_image)
     predicted_class_index = np.argmax(prediction[0])
 
     class_labels = ["Kedi","Köpek"]
 
     prediction = class_labels[predicted_class_index]
-    return {"prediction": prediction}
+    return JSONResponse(content={"sonuc": prediction})
 
 
 
